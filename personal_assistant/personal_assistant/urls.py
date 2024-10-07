42279e5d072cbd33c46acd8a497168e3c28388c6
Yurski <154272088+Yurski@users.noreply.github.com>
2024-10-08 00:00:48 +0300

    додано до проекту note

--- personal_assistant/personal_assistant/urls.py
+++ personal_assistant/personal_assistant/urls.py
@@ -27,10 +27,10 @@ urlpatterns = [
     path('files/', file_list, name='file_list'),
 
     # URL для аутентифікації
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-]
+#     path('login/', auth_views.LoginView.as_view(), name='login'),
+#     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+#     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+#     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+#     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+#     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+# ]
