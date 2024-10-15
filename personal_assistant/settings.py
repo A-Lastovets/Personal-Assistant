41f93e0d53a43cc52a2ac88e4c319abc239905b0
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-10-15 19:18:32 +0200

    media_root

--- personal_assistant/settings.py
+++ personal_assistant/settings.py
@@ -165,7 +165,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
 
