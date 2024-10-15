440c919107d65fb67625ce14b0c6e771549e51d3
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-10-16 01:50:18 +0200

    avatar5

--- users_app/signals.py
+++ users_app/signals.py
@@ -6,32 +6,30 @@ import requests
 from io import BytesIO
 from django.core.files.base import ContentFile
 
+def fetch_and_save_default_avatar(profile, username):
+    """Fetches a default avatar from a URL and saves it to the profile."""
+    default_avatar_url = 'https://sudoteam.s3.eu-north-1.amazonaws.com/default_avatar.png'
+    
+    try:
+        response = requests.get(default_avatar_url)
+        if response.status_code == 200:
+            img = BytesIO(response.content)
+            file_name = f'{username}_avatar.png'
+            profile.avatar.save(file_name, ContentFile(img.getvalue()), save=False)
+            profile.save()
+    except requests.RequestException as e:
+        print(f"Error fetching avatar from {default_avatar_url}: {e}")
+
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile.objects.create(user=instance)
-        
-        # Загрузка аватара по умолчанию с внешнего URL (Amazon S3)
-        default_avatar_url = 'https://sudoteam.s3.eu-north-1.amazonaws.com/default_avatar.png'
-        response = requests.get(default_avatar_url)
-
-        if response.status_code == 200:
-            img = BytesIO(response.content)
-            file_name = f'{instance.username}_avatar.png'
-            profile.avatar.save(file_name, ContentFile(img.getvalue()), save=False)
-
-        profile.save()
+        fetch_and_save_default_avatar(profile, instance.username)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
-    if not instance.profile.avatar:
-        # Если аватар был очищен, подставляем изображение по умолчанию
-        default_avatar_url = 'https://sudoteam.s3.eu-north-1.amazonaws.com/default_avatar.png'
-        response = requests.get(default_avatar_url)
-        
-        if response.status_code == 200:
-            img = BytesIO(response.content)
-            file_name = f'{instance.username}_avatar.png'
-            instance.profile.avatar.save(file_name, ContentFile(img.getvalue()), save=False)
+    profile = instance.profile
+    if not profile.avatar:
+        fetch_and_save_default_avatar(profile, instance.username)
     
-    instance.profile.save()
+    profile.save()
