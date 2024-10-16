fe49bc5ea8b41c1931cb8713ef7e85c55d884329
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-10-16 10:08:53 +0200

    Update models2.py

--- users_app/models.py
+++ users_app/models.py
@@ -1,21 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to='profile_images', blank=True, null=True)
+    avatar_url = models.URLField(blank=True, null=True)  # Поле для хранения URL аватара
 
     def __str__(self):
         return self.user.username
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        # Уменьшение размера изображения
-        if self.avatar:
-            img = Image.open(self.avatar.path)
-            if img.height > 250 or img.width > 250:
-                new_img = (250, 250)
-                img.thumbnail(new_img)
-                img.save(self.avatar.path)
