57bc8a987fa17f5d25c5b3f64df1b0537c2bc88c
A-Lastovets <164244571+A-Lastovets@users.noreply.github.com>
2024-10-16 10:20:43 +0200

    Update signals3.py
    remove def save_profile

--- users_app/signals.py
+++ users_app/signals.py
@@ -2,26 +2,11 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
-import requests
-from io import BytesIO
-from django.core.files.base import ContentFile
-
-def fetch_and_save_default_avatar(profile, username):
-    """Fetches a default avatar from a URL and saves it to the profile."""
-    default_avatar_url = 'https://sudoteam.s3.eu-north-1.amazonaws.com/default_avatar.png'
-    
-    try:
-        response = requests.get(default_avatar_url)
-        if response.status_code == 200:
-            img = BytesIO(response.content)
-            file_name = f'{username}_avatar.png'
-            profile.avatar.save(file_name, ContentFile(img.getvalue()), save=False)
-    except requests.RequestException as e:
-        print(f"Error fetching avatar from {default_avatar_url}: {e}")
 
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile.objects.create(user=instance)
-        fetch_and_save_default_avatar(profile, instance.username)
-        profile.save()  # Сохраняем профиль с загруженным аватаром
+        # Сохраняем URL по умолчанию
+        profile.avatar = 'https://sudoteam.s3.eu-north-1.amazonaws.com/default_avatar.png'
+        profile.save()
